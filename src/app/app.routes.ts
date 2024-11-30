d075906f886dbe8d5313d316bb3a96df3f963835
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2024-11-30 12:03:42 +0300

    created componentss for the modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: () => import('./home-module/home-module.module').then(m => m.HomeModuleModule) },
+    { path: 'auth', loadChildren: () => import('./auth-module/auth-module.module').then(m => m.AuthModuleModule) },
+    { path: 'movie', loadChildren: () => import('./movie-module/movie-module.module').then(m => m.MovieModuleModule) },
+    { path: '**', redirectTo: 'home' }
+];
